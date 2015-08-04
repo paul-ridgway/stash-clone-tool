dc94dde8290fb96cba5edc042bc979715d28e47a
Douglas Mills <slipdigital@gmail.com>
2015-08-04 11:44:33 +0100

    Updated script to take a relative path to allow the script to work when downloaded with its own hierachy

--- clone.rb
+++ clone.rb
@@ -4,48 +4,49 @@ require 'json'
 require 'colorize'
 require 'highline/import'
 
-base_url  = ask("Stash URL: ") { |q| q.echo = true }
+base_url = ask("Stash URL: ") { |q| q.echo = true }
+# Doug Added to preserve editing stash tool in place when downloaded by its self
+relative_folder = ask("Relative directory e.g. '..': )") { |q| q.echo = true }
 $username = ask("Username:  ") { |q| q.echo = true }
 $password = ask("Password:  ") { |q| q.echo = "*" }
 
-def request(url) 
-	uri = URI(url)
-
-	req = Net::HTTP::Get.new(uri)
-	req.basic_auth $username, $password
-
-	res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') {|http|
-	  http.request(req)
-	}
-	JSON.parse(res.body)
+def request(url)
+  uri = URI(url)
+  req = Net::HTTP::Get.new(uri.to_s)
+  req.basic_auth $username, $password
+  res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') { |http|
+    http.request(req)
+  }
+  JSON.parse(res.body)
 end
 
 puts 'Scanning projects...'.light_yellow
 
 projects = request("#{base_url}/rest/api/1.0/projects?limit=100")
 
-if projects['errors'] 
-	puts projects['errors'].map{|e| e['message']}.join('. ')
-	exit -1
+if projects['errors']
+  puts projects['errors'].map { |e| e['message'] }.join('. ')
+  exit -1
 end
 
 projects['values'].each do |p|
-	puts "  - #{p['name']}...".light_yellow
-	project = request("#{base_url}/rest/api/1.0/projects/#{p['key']}/repos")
-	project['values'].each do |r|
-		puts "    - #{r['name']}...".light_yellow
-		clone_links = r['links']['clone']
-		clone_link = clone_links.select{|l| l['name'] == 'ssh'}.first['href']
-		folder = "#{p['key']}/#{r['slug']}"
-		git_command = "git clone #{clone_link} #{p['key']}/#{r['slug']}"
-		puts "        Cloning ".light_blue + clone_link.light_white + " to ".light_blue + folder.light_white
-		if Dir.exists?(folder) 
-  		  puts "        Target already exists".light_red
-		else
-			puts "        #{git_command}".light_green
-			puts
-			system(git_command)
-			puts
-		end
-	end
+  puts "  - #{p['name']}...".light_yellow
+  project = request("#{base_url}/rest/api/1.0/projects/#{p['key']}/repos")
+  project['values'].each do |r|
+    puts "    - #{r['name']}...".light_yellow
+    puts r['links'].to_s
+    clone_links = r['links']['clone']
+    clone_link = clone_links.select { |l| l['name'] == 'ssh' }.first['href']
+    folder = File.join(relative_folder, p['key'], r['slug'])
+    git_command = "git clone #{clone_link} #{folder}"
+    puts "        Cloning ".light_blue + clone_link.light_white + " to ".light_blue + folder.light_white
+    if Dir.exists?(folder)
+      puts "        Target already exists".light_red
+    else
+      puts "        #{git_command}".light_green
+      puts
+      system(git_command)
+      puts
+    end
+  end
 end
